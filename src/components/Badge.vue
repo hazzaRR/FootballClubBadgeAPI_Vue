db17fbec97f19b8d8fb823eb8ef4c9ac2a7f2566
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2024-03-29 20:03:52 +0000

    created badge component which will display all the badges and team names and add tailwind to the project

---
+++ src/components/Badge.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+
+        <img :src="clubBadge.src" :alt="clubBadge.alt">
+
+        <h2>{{ teamName }}</h2>
+
+    </div>
+</template>
+
+<script setup>
+
+import {ref} from 'vue';
+
+
+const clubBadge = ref({
+    src: null,
+    alt: null
+})
+
+const teamName = ref(null);
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="text-3xl font-bold">
         homepage
     </div>
 </template>
