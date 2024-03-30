711fd4d6949c8b50cd36f80babe39af190f969eb
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2024-03-30 19:48:57 +0000

    added added border and background colour to badge component

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="rounded-lg flex flex-col justify-between">
+    <div class="rounded-lg flex flex-col justify-between border-[#011489] bg-[#BCA452] border-4">
         <div class="flex justify-center m-6 items-center">
             <img :src="`${BASE_URL}/api/badge/${teamName}?padding=true`" :alt="`${teamName} club badge`" class="">
         </div>
         
-        <h2 class="text-lg text-center w-full bg-red-100">{{ teamName }}</h2>
+        <h2 class="text-lg text-center w-full rounded-md justify-center">{{ teamName }}</h2>
 
     </div>
 </template>
