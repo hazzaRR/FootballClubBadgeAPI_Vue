0f4d5449cc31b5c10d2e09f9b901b8ce6a8eb8d1
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2024-03-30 16:21:52 +0000

    changed styling so that team name appears at the bottom of the div and is consistent for all the teams divs so they are aligned the same

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="mx-auto w-200 h-200">
-        <div class="flex justify-center items-center">
-            <img :src="`${BASE_URL}/api/badge/${teamName}?width=100&height=100`" :alt="`${teamName} club badge`" class="">
+    <div class="rounded-lg flex flex-col justify-between">
+        <div class="flex justify-center m-6 items-center">
+            <img :src="`${BASE_URL}/api/badge/${teamName}?padding=true`" :alt="`${teamName} club badge`" class="">
         </div>
-
-        <h2 class="text-lg mx-auto text-center">{{ teamName }}</h2>
+        
+        <h2 class="text-lg text-center w-full bg-red-100">{{ teamName }}</h2>
 
     </div>
 </template>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,12 +3,14 @@
         homepage
     </div>
 
-    <div class="flex">
-        <h1 class="text-2xl font-bold w-full h-full mx-auto text-center my-6">Supported Clubs</h1>
-    </div>
-
-    <div class="grid grid-cols-4 gap-4">
-        <Badge class="hover:bg-slate-200 p-4 w-100 h-100" v-for="(team, index) in teams" :key="index" :team-name="team" />
+    <div class="mx-auto w-full">
+        <div class="flex">
+            <h1 class="text-2xl font-bold w-full h-full mx-auto text-center my-6">Supported Clubs</h1>
+        </div>
+        
+        <div class="grid grid-cols-4 gap-2 m-4">
+            <Badge class="hover:bg-slate-200" v-for="(team, index) in teams" :key="index" :team-name="team" />
+        </div>
     </div>
 </template>
 
