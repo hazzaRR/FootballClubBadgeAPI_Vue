f6ee144f4ab9e56656f6cdf90d26db25fb9027d1
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2024-03-31 19:44:08 +0100

    changed endpoints to new one with .png extension and added example with jpeg call

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="rounded-lg flex flex-col justify-between border-[#011489] bg-[#BCA452] border-4">
         <div class="flex justify-center m-6 items-center">
-            <img :src="`${BASE_URL}/api/badge/${teamName}?padding=true`" :alt="`${teamName} club badge`" class="">
+            <img :src="`${BASE_URL}/api/badge/${teamName}.png?padding=true`" :alt="`${teamName} club badge`" class="">
         </div>
         
         <h2 class="text-lg text-center w-full rounded-md justify-center pb-2">{{ teamName }}</h2>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="w-full h-96 bg-[#011489] opacity-80 grid grid-cols-1 md:grid-cols-2 content-center py-10">
+    <div class="w-full h-auto bg-[#011489] opacity-80 grid grid-cols-1 md:grid-cols-2 content-center py-10">
 
         <div class="flex justify-center items-center">
             <h1 class="text-6xl font-mono font-bold text-white">Quick Start</h1>
@@ -8,11 +8,17 @@
         <div class="flex flex-col content-center md:content-start">
             <highlightjs class="m-2 w-full md:w-10/12" language='html' :code="code"/>
             <h2 class=" ml-2 text-xl text-white">Example</h2>
-            <highlightjs class="m-2 w-full md:w-10/12" language='html' :code="exampleCall"/>
+            <highlightjs class="m-2 w-full md:w-10/12" language='html' :code="exampleCallPng"/>
+            <highlightjs class="m-2 w-full md:w-10/12" language='html' :code="exampleCallJpeg"/>
 
             <h2 class="ml-2 text-xl text-white">Result:</h2>
-            <div class="flex justify-center m-2 items-center bg-white rounded-md w-1/3">
-                <img :src="`${BASE_URL}/api/badge/Derby%20County?width=100&height=100&padding=true`" :alt="`Derby County club badge`" class="">
+            <div class="flex justify-center m-2 items-center bg-gray-200 rounded-md w-1/3">
+                <img :src="`${BASE_URL}/api/badge/Derby%20County.png?width=100&height=100&padding=true`" :alt="`Derby County club badge`" class="">
+                <p class="ml-4 text-lg">png image</p>
+            </div>
+            <div class="flex justify-center m-2 items-center bg-gray-200 rounded-md w-1/3">
+                <img :src="`${BASE_URL}/api/badge/Derby%20County.jpeg?width=100&height=100&padding=true`" :alt="`Derby County club badge`" class="">
+                <p class="ml-4 text-lg">jpeg image</p>
             </div>
         </div>
     </div>
@@ -26,8 +32,9 @@ import hljsVuePlugin from "@highlightjs/vue-plugin";
 
 const highlightjs = hljsVuePlugin.component;
 
-const code = "<img src=\"https://eflBadges.com/:team?width=<int>&width<int>&padding=<boolean>&grayscale=<boolean>\">"
-const exampleCall = "<img src=\"https://localhost:3000/api/badge/Derby%20County?width=100&height=100&padding=true\">"
+const code = "<img src=\"https://eflBadges.com/:team<.png/jpeg>?width=<int>&width<int>&padding=<boolean>&grayscale=<boolean>\">"
+const exampleCallPng = "<img src=\"https://localhost:3000/api/badge/Derby%20County.png?width=100&height=100&padding=true\">"
+const exampleCallJpeg = "<img src=\"https://localhost:3000/api/badge/Derby%20County.jpeg?width=100&height=100&padding=true\">"
 
 
 </script>
